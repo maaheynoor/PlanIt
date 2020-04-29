30116e707957fe8afdb4b949f28f6d2dad3e7abe
maaheynoor <maaheynoor.s@somaiya.edu>
2020-04-29 21:44:21 +0530

    commit 1

--- venv/main.py
+++ venv/main.py
@@ -1,27 +1,19 @@
-from tkinter import *
 import speech_recognition as sr
 from gtts import gTTS
 import os
 import time
 import datetime
 import playsound
+import pyttsx3
+import re
+import psycopg2     #database
+from tkinter import *       #GUI
 
-"""
-from gtts import gTTS
-import os
-mytext="Hello Programming in Python"
-language='en'
-output=gTTS(text=mytext,lang=language,slow=False)
-output.save("output.mp3")
-os.system("start output.mp3")
-"""
-def speak(text="hello"):
-    tts = gTTS(text=text, lang='en')
-    filename = 'voice.mp3'
-    tts.save(filename)
-    #os.system("start output.mp3")
-    playsound.playsound(filename)
-    return
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
 
 def get_audio():
     r = sr.Recognizer()
@@ -30,16 +22,18 @@ def get_audio():
         said = ""
         try:
             said = r.recognize_google(audio)
-            print(said)
+            print("You:",said)
         except Exception as e:
-            print("Exception: " + str(e))
+            print("Exception: Something went wrong" + str(e))
+            speak("Something went wrong")
     return said
+
 
 #get date from text
 MONTHS = ["january", "february", "march", "april", "may", "june","july", "august", "september","october", "november", "december"]
 DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
 DAY_EXTENTIONS = ["rd", "th", "st", "nd"]
-def get_date(text):
+def DateFromText(text):
     text = text.lower()
     today = datetime.date.today()
     if text.count("today") > 0:
@@ -74,7 +68,7 @@ def get_date(text):
         else:
             month = today.month
 
-        # if we only found a dta of the week
+        # if we only found a day of the week
     if month == -1 and day == -1 and day_of_week != -1:
         current_day_of_week = today.weekday()
         dif = day_of_week - current_day_of_week
@@ -89,19 +83,157 @@ def get_date(text):
         return datetime.date(month=month, day=day, year=year)
 
 
-window=Tk()
-window.geometry("1000x500")
-window.title("Task Manager")
-fn=StringVar()
-label1=Label(window,text="Schedule your tasks with voicetm",fg="blue",font=("arial",16,"bold")).pack()
-#pack to place at center, place(x=,y=) or label1.pack(fill=BOTH,pady=,padx=) or grid(row=,column=)
-#pack(..,expand=True) Full screen filled with label
-button1=Button(window,text="Speak",fg="yellow",bg="red",relief=RIDGE,font=("arial",16,"bold"),command=lambda: get_audio()) #GROOVE,RIDGE,SUNKEN,RAISED
-button1.place(x=50,y=50)
 
-entry1=Entry(window,width=25,font=("arial",16),textvar=fn)
-entry1.place(x=50,y=100)
 
-button2=Button(window,text="Convert to speech",fg="yellow",bg="red",relief=RIDGE,font=("arial",16,"bold"),command=lambda: speak("welcome back")) #GROOVE,RIDGE,SUNKEN,RAISED
-button2.place(x=400,y=100)
-window.mainloop()
+
+class mainApp(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+
+        self._frame = None
+        self.switch_frame(StartPage)
+    def switch_frame(self, frame_class):
+        """Destroys current frame and replaces it with a new one."""
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
+
+class StartPage(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        label = Label(self, text="Manage Your Tasks using Speech")
+        label.pack()
+        taskButton = Button(self, text="Task", fg="yellow", bg="red", font=("arial", 16, "bold"),
+                                 command=lambda: master.switch_frame(TaskPage))
+        taskButton.pack()
+        todoButton = Button(self, text="To Do", fg="yellow", bg="red", font=("arial", 16, "bold"),
+                                 command=lambda: master.switch_frame(ToDoPage))
+        todoButton.pack()
+        notesButton = Button(self, text="Notes", fg="yellow", bg="red", font=("arial", 16, "bold"),
+                                 command=lambda: master.switch_frame(NotesPage))
+        notesButton.pack()
+
+
+
+
+class TaskPage(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        label1 = Label(self, text="Task Schedule:")
+        label1.pack()
+        label2 = Label(self, text="Add task using text or voice")
+        label2.pack()
+
+        self.task = StringVar()
+        frametask = Frame(self)
+        frametask.pack(fill=X)
+        labeltask = Label(frametask, text="Task:", width=6)
+        labeltask.pack(side=LEFT, padx=5, pady=5)
+        entrytask = Entry(frametask,textvariable=self.task)
+        entrytask.pack(side=LEFT,padx=5)
+        speechtask = Button(frametask,text="Speak",command=lambda: self.get_task())
+        speechtask.pack(side=LEFT, padx=5, pady=5)
+
+        self.date = StringVar()
+        framedate = Frame(self)
+        framedate.pack(fill=X)
+        labeldate = Label(framedate, text="Date:", width=6)
+        labeldate.pack(side=LEFT, padx=5, pady=5)
+        entrydate = Entry(framedate,textvariable=self.date)
+        entrydate.pack(side=LEFT, padx=5)
+        speechdate = Button(framedate, text="Speak",command=lambda: self.get_date())
+        speechdate.pack(side=LEFT, padx=5, pady=5)
+
+        frametime = Frame(self)
+        frametime.pack(fill=X)
+        labeltime = Label(frametime, text="Time:", width=6)
+        labeltime.pack(side=LEFT, padx=5, pady=5)
+        entrytime = Entry(frametime)
+        entrytime.pack(side=LEFT, padx=5, expand=True)
+        speechtime = Button(frametime, text="Speak",command=lambda: self.get_time())
+        speechtime.pack(side=LEFT, padx=5, pady=5)
+
+        addtask=Button(self,text="Add Task")
+        addtask.pack()
+        todoButton = Button(self, text="To Do", fg="yellow", bg="red", font=("arial", 16, "bold"),
+                                 command=lambda: master.switch_frame(ToDoPage))
+        todoButton.pack()
+        notesButton = Button(self, text="Notes", fg="yellow", bg="red", font=("arial", 16, "bold"),
+                                  command=lambda: master.switch_frame(NotesPage))
+        notesButton.pack()
+
+    def get_task(self):
+        text = get_audio()
+        self.task.set(text)
+
+    def get_date(self):
+        text = get_audio()
+        tdate=DateFromText(text)
+        self.date.set(tdate)
+
+    def get_time(self):
+        text = get_audio()
+        tdate=DateFromText(text)
+        self.date.set(tdate)
+
+
+class ToDoPage(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        label = Label(self, text="To Do page")
+        label.pack()
+
+        self.todo = StringVar()
+        frametodo1 = Frame(self)
+        frametodo1.pack(fill=X)
+        entrytodo = Entry(frametodo1,textvariable=self.todo)
+        entrytodo.pack(side=LEFT,padx=5)
+        speechtodo = Button(frametodo1, text="Speak",command=lambda: self.get_todo())
+        speechtodo.pack(side=LEFT, padx=5, pady=5)
+        addtodo = Button(self, text="Add to To-Do List")
+        addtodo.pack()
+
+        taskButton = Button(self, text="Task", fg="yellow", bg="red", font=("arial", 16, "bold"),
+                                 command=lambda: master.switch_frame(TaskPage))
+        taskButton.pack()
+        notesButton = Button(self, text="Notes", fg="yellow", bg="red", font=("arial", 16, "bold"),
+                                  command=lambda: master.switch_frame(NotesPage))
+        notesButton.pack()
+
+    def get_todo(self):
+        text = get_audio()
+        self.todo.set(text)
+
+class NotesPage(Frame):
+    def __init__(self,master):
+        Frame.__init__(self, master)
+        label = Label(self, text="Notes Page")
+        label.pack()
+
+        self.note = StringVar()
+        framenote1 = Frame(self)
+        framenote1.pack(fill=X)
+        entrynote = Entry(framenote1,textvariable=self.note)
+        entrynote.pack(side=LEFT, padx=5)
+        speechnote = Button(framenote1, text="Speak",command=lambda: self.get_note())
+        speechnote.pack(side=LEFT, padx=5, pady=5)
+        addnote = Button(self, text="Add to Notes")
+        addnote.pack()
+
+        taskButton = Button(self, text="Task", fg="yellow", bg="red", font=("arial", 16, "bold"),
+                                 command=lambda: master.switch_frame(TaskPage))
+        taskButton.pack()
+        todoButton = Button(self, text="To Do", fg="yellow", bg="red", font=("arial", 16, "bold"),
+                                 command=lambda: master.switch_frame(ToDoPage))
+        todoButton.pack()
+
+    def get_note(self):
+        text = get_audio()
+        self.note.set(text)
+
+if __name__ == "__main__":
+    root = mainApp()
+    root.title("Task Manager")
+    root.mainloop()
