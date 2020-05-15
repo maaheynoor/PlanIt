45cc9a2ace11d142ddf3fcf3ef7675c276464757
alfiza-shaikh <50302756+alfiza-shaikh@users.noreply.github.com>
2020-05-15 18:09:52 +0530

    horizontal scrollbar added

--- scroll.py
+++ scroll.py
@@ -5,13 +5,17 @@ class ScrollFrame(tk.Frame):
     def __init__(self, parent, width, height):
         super().__init__(parent)  # create a frame (self)
 
-        self.canvas = tk.Canvas(self, borderwidth=0, width=width, height=height, background="#ffffff")  # place canvas on self
+        self.canvas = tk.Canvas(self, borderwidth=0, width=width, height=height,
+                                background="#ffffff")  # place canvas on self
         self.viewPort = tk.Frame(self.canvas,
                                  background="#ffffff")  # place a frame on the canvas, this frame will hold the child widgets
         self.vsb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)  # place a scrollbar on self
+        self.hsb = tk.Scrollbar(self, orient="horizontal", command=self.canvas.xview)  # place a scrollbar on self
         self.canvas.configure(yscrollcommand=self.vsb.set)  # attach scrollbar action to scroll of canvas
+        self.canvas.configure(xscrollcommand=self.hsb.set)  # attach scrollbar action to scroll of canvas
 
         self.vsb.pack(side="right", fill="y")  # pack scrollbar to right of self
+        self.hsb.pack(side="bottom", fill="x")  # pack scrollbar to right of self
         self.canvas.pack(side="left", fill="both", expand=True)  # pack canvas to left of self and expand to fil
         self.canvas_window = self.canvas.create_window((0, 0), window=self.viewPort, anchor="nw",
                                                        # add view port frame to canvas
